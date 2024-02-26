a0a8a61c2f97628dae073ff9b91ffcdaab1c750a
MoonCress <alfoga02@ucm.es>
2024-02-26 19:26:59 +0100

    Mini cambio

--- src/boot.js
+++ src/boot.js
@@ -32,7 +32,7 @@ export default class Boot extends Phaser.Scene {
     /*this.load.image('platform', platform);
     this.load.image('base', base);
     this.load.image('star', star)*/
-    //this.load.image('player', player);
+    this.load.image('player', player);
     this.load.spritesheet('player_spritesheet', 
                            playerSpritesheet,
                            {frameWidth: 32, frameHeight: 32, endFrame: 12});
