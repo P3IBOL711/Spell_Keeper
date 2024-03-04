a3867214d7eb04d58809d0faeacb8cc4e40a2ab5
gonzalete1 <gonzalobd1609@gmail.com>
2024-03-04 11:07:03 +0100

    Prototipo generacion

---
+++ src/rooms/ar_r6.js
@@ -0,0 +1,20 @@
+export default ar_r6 = {
+
+    name: "ar_r6",
+    
+    level: "armory",
+    
+    path: "../../assets/armory/ar_r6.tmx",
+    
+    entrance: false,
+    exit: false,
+    empty: false, //Habitacion vacia
+    deadend: true,//para saber que solo tiene una salida
+    shop:false,
+    
+    door_north: false,
+    door_south: false,
+    door_east: true,
+    door_west: false
+    
+    };
---
+++ src/rooms/ar_x1.js
@@ -0,0 +1,20 @@
+export default ar_x1 = {
+
+    name: "ar_x1",
+    
+    level: "armory",
+    
+    path: "../../assets/armory/ar_x1.tmx",                          
+    
+    entrance: false,
+    exit: true,
+    empty: false, //Habitacion vacia
+    deadend: true,//para saber que solo tiene una salida
+    shop:false,
+    
+    door_north: false,
+    door_south: true,
+    door_east: false,
+    door_west: false
+    
+    };
