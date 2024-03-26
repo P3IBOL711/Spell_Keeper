75448f2e709a26ffad31bfd31200ce478d62f579
Pablo <pablofol2003@gmail.com>
2024-03-26 16:33:03 +0100

    standard skeleton done

--- src/enemies/standardSkeleton.js
+++ src/enemies/standardSkeleton.js
@@ -17,7 +17,6 @@ export default class StandardSkeleton extends Enemy {
     constructor(scene, x, y, target) {
         super(scene, x, y, 'standardSkeleton');
 
-
         this.anims.create({
             key: 'walking',
             frames: this.anims.generateFrameNumbers('standardSkeletonSpritesheet', { start: 0, end: 7 }),
@@ -28,8 +27,8 @@ export default class StandardSkeleton extends Enemy {
         this.anims.create({
             key: 'attack',
             frames: this.anims.generateFrameNumbers('standardSkeletonSpritesheet', { start: 8, end: 10 }),
-            frameRate: 10,
-            repeat: -1
+            frameRate: 7,
+            repeat: 0
         });
 
         this.anims.create({
@@ -38,6 +37,15 @@ export default class StandardSkeleton extends Enemy {
             frameRate: 5,
             repeat: 0
         });
+
+        this.timerAttack = this.scene.time.addEvent({
+            delay: 1500,
+            callback: this.onTimerAttack,
+            callbackScope: this,
+            loop: true
+        });
+
+        this.timerAttack.paused = true;
 
         this.setScale(3);
 
@@ -54,16 +62,33 @@ export default class StandardSkeleton extends Enemy {
         // SE PODRIA MEJORAR CON this.on(animationstart) PERO NO SABEMOS HACERLO
         this.on(Phaser.Animations.Events.ANIMATION_START, () => {
             if (this.anims.getName() === 'attack'){
+                this.body.setVelocity(0);
                 this.attackZone = new HitBox(this.scene, this.x + (this.flipX ? -65 : 65), this.y - 10, 60, 120, this.target, this.damage);
+            }
+        })
+
+        this.on(Phaser.Animations.Events.ANIMATION_COMPLETE, () => {
+            if (this.anims.getName() === 'attack'){
+                this.attackZone.destroy(true);
+                this.play('walking', true)
+                this.scene.physics.moveToObject(this, this.target, this.speed);
             }
         })
 
         this.on(Phaser.Animations.Events.ANIMATION_STOP, () => {
             if (this.anims.getName() === 'attack'){
                 this.attackZone.destroy(true);
+                this.play('walking', true)
+            }
+            else if(this.anims.getName() === 'walking'){
+                this.play('walking', true);
             }
         })
 
+    }
+
+    onTimerAttack(){
+        this.play('attack');
     }
 
     /**
@@ -80,16 +105,16 @@ export default class StandardSkeleton extends Enemy {
         if (this.life > 0) {
             this.body.setOffset(this.width * (this.flipX ? 0.4 : 0.42), this.height * 0.34);
 
-            if (Phaser.Math.Distance.Between(this.x, this.y, this.target.x, this.target.y) >= 50){
+            if (Phaser.Math.Distance.Between(this.x, this.y, this.target.x, this.target.y) >= 100){
+                this.timerAttack.paused = true;
                 this.play('walking', true);
                 this.scene.physics.moveToObject(this, this.target, this.speed);
             }
             else {
                 // creáis la zone de ataque
                 // cambiáis la animación (que ya está)
-                this.play('attack', true);
-                this.body.setVelocity(0);
-                
+                this.timerAttack.paused = false;
+                this.playAfterRepeat('walking');
             } 
         }
     }
