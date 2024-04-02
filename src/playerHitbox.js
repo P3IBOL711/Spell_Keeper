dadf37c7622bdb123d854763a76adaa9fe0f13d0
MoonCress <alfoga02@ucm.es>
2024-04-02 23:05:05 +0200

    hotfix again :')

--- src/playerHitbox.js
+++ src/playerHitbox.js
@@ -21,7 +21,6 @@ export default class PlayerHitBox extends Phaser.GameObjects.Zone {
         this.scene.physics.add.overlap(this.scene.enemies, this, (enemy) => {
             enemy.receiveDamage(damage);
             this.scene.player.regenMana();
-            console.log('overlapped');
         });
     }
 }
