e28b30301bd0998409b0749537dea110addc3864
gonzalete1 <gonzalobd1609@gmail.com>
2024-04-14 20:00:47 +0200

    Subidas mas cosas

---
+++ assets/armory/tiles/Base.tsx
@@ -0,0 +1,69 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.2" name="Base" tilewidth="32" tileheight="32" tilecount="20" columns="5">
+ <image source="../../Pixel Art PNGs/Armory_Tileset.png" width="160" height="128"/>
+ <tile id="5">
+  <properties>
+   <property name="Walkable" type="bool" value="false"/>
+  </properties>
+ </tile>
+ <tile id="6">
+  <properties>
+   <property name="Walkable" type="bool" value="false"/>
+  </properties>
+ </tile>
+ <tile id="7">
+  <properties>
+   <property name="Walkable" type="bool" value="false"/>
+  </properties>
+ </tile>
+ <tile id="8">
+  <properties>
+   <property name="Walkable" type="bool" value="false"/>
+  </properties>
+ </tile>
+ <tile id="9">
+  <properties>
+   <property name="Walkable" type="bool" value="false"/>
+  </properties>
+ </tile>
+ <tile id="10">
+  <properties>
+   <property name="Walkable" type="bool" value="false"/>
+  </properties>
+ </tile>
+ <tile id="11">
+  <properties>
+   <property name="Walkable" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="12">
+  <properties>
+   <property name="Walkable" type="bool" value="false"/>
+  </properties>
+ </tile>
+ <tile id="13">
+  <properties>
+   <property name="Walkable" type="bool" value="false"/>
+  </properties>
+ </tile>
+ <tile id="14">
+  <properties>
+   <property name="Walkable" type="bool" value="false"/>
+  </properties>
+ </tile>
+ <tile id="15">
+  <properties>
+   <property name="Walkable" type="bool" value="false"/>
+  </properties>
+ </tile>
+ <tile id="16">
+  <properties>
+   <property name="Walkable" type="bool" value="false"/>
+  </properties>
+ </tile>
+ <tile id="17">
+  <properties>
+   <property name="Walkable" type="bool" value="false"/>
+  </properties>
+ </tile>
+</tileset>
---
+++ assets/library/tiles/Base.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.2" name="Base" tilewidth="32" tileheight="32" tilecount="80" columns="10">
+ <image source="../../Pixel Art PNGs/Library_Tileset .png" width="320" height="256"/>
+</tileset>
