740b40150a83c522e13625ed808d6c1ce3349168
gonzalete1 <gonzalobd1609@gmail.com>
2024-05-06 12:32:14 +0200

    hotfix

--- src/enemies/bossTree/bossTree.js
+++ src/enemies/bossTree/bossTree.js
@@ -224,22 +224,22 @@ export default class BossTree extends Enemy {
             }
         }
     }
-}
-/**
- * Métodos preUpdate de Phaser. En este caso solo se encarga del movimiento del jugador.
- * Como se puede ver, no se tratan las colisiones con las estrellas, ya que estas colisiones 
- * ya son gestionadas por la estrella (no gestionar las colisiones dos veces)
- * @override
- */
-preUpdate(t, dt) {
-    // IMPORTANTE: Si no ponemos esta instrucción y el sprite está animado
-    // no se podrá ejecutar la animación del sprite. 
-    super.preUpdate(t, dt);
-}
 
-flipEnemy(){ }
+    /**
+     * Métodos preUpdate de Phaser. En este caso solo se encarga del movimiento del jugador.
+     * Como se puede ver, no se tratan las colisiones con las estrellas, ya que estas colisiones 
+     * ya son gestionadas por la estrella (no gestionar las colisiones dos veces)
+     * @override
+     */
+    preUpdate(t, dt) {
+        // IMPORTANTE: Si no ponemos esta instrucción y el sprite está animado
+        // no se podrá ejecutar la animación del sprite. 
+        super.preUpdate(t, dt);
+    }
 
-isProjectile(){
-    return false;
-}
+    flipEnemy() { }
+
+    isProjectile() {
+        return false;
+    }
 }
