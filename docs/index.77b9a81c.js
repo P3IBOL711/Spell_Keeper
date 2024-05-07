0ce7aff62664923270f6dae5a8f983cb3580673a
Sandra <sandracg1234@gmail.com>
2024-05-07 20:32:02 +0200

    intento de run build

---
+++ docs/index.77b9a81c.js
@@ -0,0 +1,2 @@
+const[o,l,r]=[208,148,234],d=document.querySelector("body");window.addEventListener("scroll",()=>{let n=1+window.scrollY/175,e=1+window.scrollY/250,[c,t,w]=[o/n,l/(1+window.scrollY/150),r/e].map(Math.round);d.style.backgroundColor=`rgb(${c}, ${t}, ${w})`});
+//# sourceMappingURL=index.77b9a81c.js.map
