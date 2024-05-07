9268c3725dc593455223d3ceba0f8d148f0d7b96
gonzalete1 <gonzalobd1609@gmail.com>
2024-05-07 18:34:56 +0200

    build

--- src/scenes/room.js
+++ src/scenes/room.js
@@ -85,7 +85,7 @@ export default class Room extends Phaser.Scene {
                 weaponMult: 1,       // Multiplier for weapon damage
                 moveSpeed: 0,        // Player movement speed
                 lck: 0,              // Player luck stat
-                MeleeWeaponArray: [new ChargeSword(this, 0, 0, 1)], // Array to store melee weapons
+                MeleeWeaponArray: [new dagger(this, 0, 0, 1)], // Array to store melee weapons
                 RangedWeaponArray: [new FireStaff(this, 0, 0, 1)],// Array to store ranged weapons
                 ActMelIndex: 0,      // Index of the currently active melee weapon
                 ActRangIndex: 0,     // Index of the currently active ranged weapon
